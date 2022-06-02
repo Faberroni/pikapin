0bc7068d7c34a95744ec9c2ed29db50e7a193c28
Muhammad Ariq <ariqakbar3435@gmail.com>
2022-06-02 10:16:18 +0800

    baru

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -5,3 +5,11 @@ const Color bgColor = Color(0xFFFBFBFD);
 
 const double defaultPadding = 16.0;
 const double defaultBorderRadius = 12.0;
+
+const kPrimaryColor = Color(0xFFFF7643);
+const kPrimaryLightColor = Color(0xFFFFECDF);
+const kPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+);
